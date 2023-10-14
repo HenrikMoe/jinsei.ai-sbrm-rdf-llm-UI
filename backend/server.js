d82e7581bc99f70aa07a0fe54746248e0a202d55
HenrikMoe <henrikmoe29@gmail.com>
2023-10-14 15:44:35 -0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // You can change the port as needed
+
+// Define a route that doesn't require authentication
+app.get('/api/some-route', (req, res) => {
+  // Your route logic goes here
+  res.json({ message: 'This route does not require authentication.' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
