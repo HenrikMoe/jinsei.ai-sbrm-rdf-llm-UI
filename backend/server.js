3dda69a4fc30487204709e39b270222ea3d34e79
HenrikMoe <henrikmoe29@gmail.com>
2023-10-14 16:27:43 -0700

    ci/cd setup

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000; // You can change the port as needed
 // Define a route that doesn't require authentication
 app.get('/api/some-route', (req, res) => {
   // Your route logic goes here
-  res.json({ message: 'This route does not require authentication.' });
+  res.json({ message: 'This route does not require authentication. change' });
 });
 
 app.listen(port, () => {
