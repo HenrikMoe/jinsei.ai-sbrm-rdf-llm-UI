92064b2531d3f536c7ec6eb9d77f864a65ecdc26
HenrikMoe <henrikmoe29@gmail.com>
2023-10-14 19:47:21 -0700

    site updates

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors'); // Import the 'cors' middleware
 const app = express();
 const port = process.env.PORT || 3000; // You can change the port as needed
+
+// Use the 'cors' middleware to enable CORS for all routes
+app.use(cors());
 
 // Define a route that doesn't require authentication
 app.get('/api/some-route', (req, res) => {
   // Your route logic goes here
-  res.json({ message: 'This route does not require authentication.1' });
+  res.json({ message: 'This route does not require authentication.' });
 });
 
 app.listen(port, () => {
