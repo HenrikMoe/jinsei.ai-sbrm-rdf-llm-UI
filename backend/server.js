766d11f5fd4c7216d4c010c10a65123c31d9b6af
HenrikMoe <henrikmoe29@gmail.com>
2023-10-15 12:01:27 -0700

    ci/cd

--- backend/server.js
+++ backend/server.js
@@ -15,3 +15,31 @@ app.get('/api/some-route', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { MONGODB_URI } = process.env;
+
+// Create a MongoClient with the MongoDB URI and configure the Stable API version
+const client = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
