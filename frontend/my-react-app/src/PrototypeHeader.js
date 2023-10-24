aaa94b21f0a3614e939240d6e7380f18850f976c
HenrikMoe <henrikmoe29@gmail.com>
2023-10-23 18:11:34 -0700

    moving the update functinality into the header and starting project level managment

--- frontend/my-react-app/src/PrototypeHeader.js
+++ frontend/my-react-app/src/PrototypeHeader.js
@@ -7,6 +7,8 @@ import Popup from './Popup';
 import Upload from './Upload';
 import Delete from './Delete'
 import Project from './Project'
+import Transform from './Transform'
+import Pipeline from './Pipeline'
 //<Project onFileUpload={onFileUpload}/>
 // <Upload onFileUpload={onFileUpload}/>
 // <Delete onFileUpload={onFileUpload} />
@@ -16,17 +18,46 @@ const PrototypeHeader = ({onFileUpload}) => {
   const [xlsxData, setXLSXData] = useState(null);
 console.log(onFileUpload)
 
+const [title, setTitle] = useState('File 1'); // Initial title
+const [org, setOrg] = useState('AB Co'); // Initial title
+
+
+const handleTitleChange = (event) => {
+  setTitle(event.target.value);
+};
+
+const handleOrgChange = (event) => {
+  setOrg(event.target.value);
+};
+
   return (
     <div className={`prototype-header ${isDarkMode ? 'dark-mode' : ''}`}>
       <img className={`header-image ${isDarkMode ? 'dark-mode' : ''}`} src={image} alt="My Image" />
-      <div className='title'>Luciano - git:main | version 0.78</div>
+      <div className='title'>Luciano - git:main | version 0.80</div>
+
       <div className='button-wrap'>
 
         <Popup onFileUpload={onFileUpload} />
         <Upload />
+        <Transform />
         <Delete  />
+        <Pipeline />
 
       </div>
+      <div>Share</div>
+      <div> <input
+        type="text"
+        className="title-input"
+        value={org}
+        onChange={handleOrgChange}
+      /> Org</div>
+      <div> <input
+        type="text"
+        className="title-input"
+        value={title}
+        onChange={handleTitleChange}
+      /> FileTree</div>
+
     </div>
   );
 };
---
+++ frontend/my-react-app/src/Transform.js
@@ -0,0 +1,37 @@
+
+// Popup.js
+import { React, useState, useEffect } from 'react';
+import './Upload.css';
+import { useDarkMode } from './DarkModeContext';
+import XLSXFileHandler from './XLSXFileHandler';
+import CSVFileHandler from './CSVFileHandler'
+import JSONfileHandler from './JSONfileHandler'
+
+
+const Transform = ({ updateForm }) => {
+  const { isDarkMode } = useDarkMode();
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className={`popup-container ${isDarkMode ? 'dark-mode' : ''}`}>
+    <div className={`dropdown ${isDropdownOpen ? 'open' : ''}`}>
+      <button className="dropdown-button" onClick={toggleDropdown}>
+        Transform &or;
+      </button>
+      <div className="dropdown-content">
+        <XLSXFileHandler  />
+        <CSVFileHandler />
+        <JSONfileHandler />
+        {/* Add other dropdown items here */}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Transform;
