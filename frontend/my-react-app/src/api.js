7fe066fe6aa470185929517647af00063deae82f
HenrikMoe <henrikmoe29@gmail.com>
2023-11-08 16:23:11 -0800

    userinfo saved to mongo

--- frontend/my-react-app/src/api.js
+++ frontend/my-react-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 let backendUrl;
 
-const backendUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+backendUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
 
 
 const api = {
