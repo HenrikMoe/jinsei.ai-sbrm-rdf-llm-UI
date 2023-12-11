7139e52daa0324ca39cc2716322f1521f1c3515d
HenrikMoe <henrikmoe29@gmail.com>
2023-12-11 15:53:29 -0800

    terms of service update

--- frontend/my-react-app/src/TermOfService.js
+++ frontend/my-react-app/src/TermOfService.js
@@ -4,20 +4,20 @@ import './TermsOfService.css'; // Import the CSS file
 const TermsOfService = () => {
   return (
     <div className="center-div">
-      <h1>Terms of Service</h1>
-      <p>
+      <h1 className="aboutJinsei">Terms of Service</h1>
+      <p className="aboutJinsei">
         By using our services, you agree to comply with these Terms of Service. Please read them carefully.
       </p>
-      <p>
+      <p className="aboutJinsei">
         You must be at least 18 years old to use our services. You are responsible for maintaining the security of your account.
       </p>
-      <p>
+      <p className="aboutJinsei">
         We reserve the right to terminate or suspend your account at any time for any reason.
       </p>
-      <p>
+      <p className="aboutJinsei">
         These terms may change over time, and any changes will be posted on our website.
       </p>
-      <p>
+      <p className="aboutJinsei">
         Please review our full Terms of Service for more details.
       </p>
     </div>
