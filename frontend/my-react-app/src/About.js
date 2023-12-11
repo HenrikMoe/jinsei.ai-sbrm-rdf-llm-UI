2102558c5b6d7f9219924d67393716249ba7fe4c
HenrikMoe <henrikmoe29@gmail.com>
2023-12-11 15:51:25 -0800

    about page

--- frontend/my-react-app/src/About.js
+++ frontend/my-react-app/src/About.js
@@ -4,10 +4,25 @@ import './About.css'; // Import the CSS file
 const About = () => {
   return (
     <div className="center-div">
-      <h1>About Jinsei</h1>
-      <p>
-      yuspchappv
-      </p>
+      <h1 className='aboutJinsei'>About Jinsei Corporation</h1>
+      <div className='aboutJinsei'>
+      Jinsei Corporation (Jinsei.ai) develops products centered around empowering standardized financial information models across multiple business domains.
+      </div>
+      <div className='aboutJinsei'>
+      Intuitive interfaces and APIs across Jinsei Corp's product suite enable businesses to create and manage standards based accounting reports, machine readable plus logic verification report processes, as well as business ontology visualizations and analytics.
+      </div>
+      <div className='aboutJinsei'>
+      Disclsoure models from US GAAP, IFRS, SEC's XBRL, and SBRM are available in Jinsei.ai's Luciano.
+      </div>
+      <div className='aboutJinsei'>
+      Business ontology models such as OMG Fibo, ACUTS, and more are under development.
+      </div>
+      <div className='aboutJinsei'>
+      Document upload LLM's and GPT Bot assisted contextual inforamtion is available throughout every process in Jinsei Corp's products.  </div>
+      <div className='aboutJinsei'>
+      Contact Henrik Moe at henrik@jinsei.ai.
+      </div>
+
     </div>
   );
 };
