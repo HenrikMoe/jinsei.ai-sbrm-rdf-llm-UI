1dc1310e5cb667ec947d15557df957fd00efb552
HenrikMoe <henrikmoe29@gmail.com>
2023-12-11 15:54:48 -0800

    privacy policy updated

--- frontend/my-react-app/src/PrivacyPolicy.js
+++ frontend/my-react-app/src/PrivacyPolicy.js
@@ -4,20 +4,20 @@ import './PrivacyPolicy.css'; // Import the CSS file
 const PrivacyPolicy = () => {
   return (
     <div className="center-div">
-      <h1>Privacy Policy</h1>
-      <p>
+      <h1 className='aboutJinsei'>Privacy Policy</h1>
+      <p className='aboutJinsei'>
         Your privacy is important to us. This Privacy Policy explains how we collect, use, and disclose your personal information.
       </p>
-      <p>
+      <p className='aboutJinsei'>
         We may collect information that you provide to us, such as your name and email address, when you interact with our services.
       </p>
-      <p>
+      <p className='aboutJinsei'>
         We use your personal information to improve and personalize our services, as well as to communicate with you.
       </p>
-      <p>
+      <p className='aboutJinsei'>
         We do not share your personal information with third parties unless required by law.
       </p>
-      <p>
+      <p className='aboutJinsei'>
         Please review our full Privacy Policy for more details.
       </p>
     </div>
