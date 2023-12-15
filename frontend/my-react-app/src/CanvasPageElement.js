cd46819907ee31c633788a3abea0d3b0f5970dc3
HenrikMoe <henrikmoe29@gmail.com>
2023-12-14 17:11:14 -0800

    pre konva sketches

--- frontend/my-react-app/src/CanvasPageElement.js
+++ frontend/my-react-app/src/CanvasPageElement.js
@@ -11,19 +11,49 @@ const PrototypeSideMenu = ({ handlePipelineInstanceSelection, sheetTitles, onShe
 
      <div className="elementTitle10">Maps
 
+     <div className='ontologyheader'>
+     <div className='notes'>Ontology Upoad </div>
+     <div className='notes'>Config Summary  </div>
+
+     <div className='notes'>Model: LEI  </div>
+     <div className='notes'> <input type="checkbox" /></div>
+
+     <div className='notes'>Model: Fibo </div>
+     <div className='notes'> <input type="checkbox" /></div>
+
+     <div className='notes'>Model: ACTUS </div>
+     <div className='notes'> <input type="checkbox" /></div>
+     <div className='elementDetailWrap'>
+     <div className='notes2'>Element Details:</div>
+
+     <div className='ontologyheader2'>
+
+     <div className='notes2'>Semantic Description:  </div>
+     <div className='notes2'>[desc]</div>
+
+     <div className='notes3'>ACTUS Role: [role] </div>
+     <div className='notes2'>Flow<input type="checkbox" /></div>
+
+     <div className='notes3'>FIbo Role: [role] </div>
+     <div className='notes2'> Flow<input type="checkbox" /></div>
+
+     <div className='notes3'>LEI Role: [role] </div>
+     <div className='notes2'> Flow<input type="checkbox" /></div>
+
+     </div>
+     </div>
+
+     </div>
+
+
+
+
+     <div className='notes2'>Konva of revenue operations, basically product terms sheets with quantity data. Multi-brnach companies - the ai will train to aggreagate the product and quantity functions of revenue for these companies and likewise with costs into the standard visualization. </div>
+
      <div className='balance-sheet2'>
 
 
-    <div className='notes'>Ontology Upoad </div>
-    <div className='notes'>Model: LEI  </div>
-    <div className='notes'>Model: Fibo </div>
-    <div className='notes'>Model: ACTUS </div>
-
-    <div className='notes'>Element: ACTUS </div>
-
-
-    <div className='notes2'>Konva of revenue operations, basically product terms sheets with quantity data. Multi-brnach companies - the ai will train to aggreagate the product and quantity functions of revenue for these companies and likewise with costs into the standard visualization. </div>
-   </div>
+    </div>
 
        </div>
    );
