a4168366aab8572ece20d76e7a1feada36a1c3c4
HenrikMoe <henrikmoe29@gmail.com>
2024-01-02 19:45:28 -0800

    much more updates soon

--- frontend/my-react-app/src/Footer.js
+++ frontend/my-react-app/src/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'; // Import the CSS file for styling
 const Footer = () => {
   return (
     <div className="footer">
-      &copy; 2023 Jinsei Corp. All rights reserved.
+      &copy; 2024 Jinsei Corp. All rights reserved.
     </div>
   );
 };
