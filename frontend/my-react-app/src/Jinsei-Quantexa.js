b405ad2a61fa223d7ddf99d03b46e09275064d61
HenrikMoe <henrikmoe29@gmail.com>
2024-01-06 18:51:46 -0800

    high as a roofg

--- frontend/my-react-app/src/Jinsei-Quantexa.js
+++ frontend/my-react-app/src/Jinsei-Quantexa.js
@@ -4,19 +4,154 @@ import './About.css'; // Import the CSS file
 const About = () => {
   return (
     <div className="center-div">
-      <h1 className='aboutJinsei'>Quantexa-Jinsei</h1>
+      <h1 className='aboutJinsei'>Jinsei.ai Quantexa Partnership</h1>
       <div className='aboutJinsei'>
-      Jinsei Corporation (Jinsei.ai) is partnering with groups at the forefront of integrating multi-threaded AI and logic based financial mangement software.
+      Author: Henrik Moe
       </div>
-      <div className='aboutJinsei'>
-      Jinsei.ai develops products centered around empowering vertical technology stacks with a coupling of standardized financial information models and modern licensing, APIs, and UXs.
+
+      <div className='tradeSecretTitle'>
+      Copyright 2024
       </div>
+
+      <div className='tradeSecretTitle'>
+      Protected under the U.S. Trade Secrets Act:
+      </div>
+
       <div className='aboutJinsei'>
-      Independently developing and testing hundreds of document cleaning LLMs, deep context GPT dialogue bots, knowledge-graph based fact verification programs, and more deep fintech.
+      <a href='https://www.congress.gov/bill/114th-congress/senate-bill/1890'>Trade Secrets</a>
+
+      </div>
+
+      <div className='tocTitle'>
+      Contents:
+      </div>
+
+      <div className='tocItem'>
+      <a href='#Intro'>Intro</a>
+      </div>
+
+      <div className='tocItem'>
+      <a href='#Groups'>Groups</a>
+      </div>
+
+      <div className='tocItem'>
+      <a href='#Projects'>Projects</a>
+      </div>
+
+      <div className='tocItem'>
+      <a href='#RiskManagement'>Risk Management</a>
+      </div>
+
+      <div className='tocItem'>
+      <a href='#Novelty'>Novelty</a>
+      </div>
+
+      <div className='tocItem'>
+      <a href='#Transactions'>Transactions</a>
+      </div>
+
+      <div className='tocItem'>
+      <a href='#Exits'>Exits</a>
+      </div>
+
+      <h2 className='topicTitle' id='Intro'>
+Intro
+      </h2>
+      <div className='aboutJinsei2'>
+      Vertical talk, ai specialization products, rolling implementations and products
+      </div>
+
+      <div className='topicTitle' id='Groups'>
+      Groups
       </div>
       <div className='aboutJinsei2'>
-      Contact for partnership outreach: henrik@jinsei.ai.
+      SBRM:
+      AuditChain Labs
+      Analytics:
+      Oxford Semantics, DFRNT
       </div>
+
+      <div className='topicTitle' id='RiskManagement'>
+      Risk Management
+      </div>
+      <div className='aboutJinsei2'>
+      Distribution for them, roll over shares
+    Rollover equity refers to a scenario where the existing shareholders of a target company (the company being acquired) are offered the opportunity to exchange their shares for equity ownership in the acquiring company or the newly combined entity that results from the transaction
+    jinsei-quantexa corp shares to quantexa - for some shares in quantexa
+    to auditchain labs then contginetn that on a roll over with quantexa?
+    strucutre so multiple companies can enter for different product niches like iris and oxford and
+    Entity solution introduced here
+
+      </div>
+
+      <div className='topicTitle' id='Novelty'>
+Novelty
+      </div>
+      <div className='aboutJinsei2'>
+      quantexa gets a 1 year exclusivity on jinsei Ip when the product lands in the vertical
+    whats auditchain’s vertical like? any? - for jason
+
+
+      </div>
+
+      <div className='topicTitle' id='Transactions'>
+Transactions
+      </div>
+      <div className='aboutJinsei2'>
+      Transaction scenarios for each product vertical
+    Jinsei-Quantexa-Auditchain
+    Jinsei-Quantexa-OxfordSemantics-DFRNT
+
+    Multiple product entities are available here with rolling equity
+      </div>
+
+      <div className='topicTitle' id='Exits'>
+      Exits
+      </div>
+      <div className='aboutJinsei2'>
+      exit plan for them
+Exit by performance after 1 year from a vertical completion. Exit by distributor license via Quantexa, NAIC for next scale.
+novelty and protection
+client numbers and revenue data will help articulate the levearge of jinsei.ai’s ai products and report leveraging
+risk for risk roll over shares of their company for my company
+must reduce risk after first cash flow
+exit for early investors
+select product
+select one bunny
+the information i need is marketing data
+disappating risk in the corporation
+theyve got the distrubution i have the product
+theres no risk for them
+exclsuistivty after product landings
+      </div>
+
+      <div className='topicTitle' id='Products'>
+      Products
+      </div>
+      <div className='aboutJinsei2'>
+      1 Process Deployment UI Management
+
+    2 FormSheet Model Population
+
+    3 FormSheet Upload To Model AI Processing
+
+    4 Logical Auditing Processes: AuditChain Paccioli Integration
+
+    5 Entity and Infliction Graph Canvas
+
+    6  Contextual Chat Assistance
+
+    vertical support
+    Rolling implementations and products
+    mermaid, react, api, xule, xbrl,  process management
+    canvas, model library, ai docuementation, chat
+    auditchain-jinsei ai modules + luca + luciano sbrm and more reporting software for quantexa vertical
+    Ontology libraries from oxford semantics and dfrnt + jinsei.ai + api/ui distribution
+      </div>
+
+
+
+
 
     </div>
   );
