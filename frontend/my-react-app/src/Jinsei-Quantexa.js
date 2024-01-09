aaa9b316c85a2bdfbc371c14b2af64cdae2c752d
HenrikMoe <henrikmoe29@gmail.com>
2024-01-08 20:40:39 -0800

    agreement

--- frontend/my-react-app/src/Jinsei-Quantexa.js
+++ frontend/my-react-app/src/Jinsei-Quantexa.js
@@ -4,7 +4,7 @@ import './About.css'; // Import the CSS file
 const About = () => {
   return (
     <div className="center-div">
-      <h1 className='aboutJinsei'>Quantexa-Jinsei.ai  Partnership</h1>
+      <h1 className='aboutJinsei'>Quantexa, AuditChain Labs, Jinsei.ai - Joint Product Development and Distribution Venture</h1>
       <div className='aboutJinsei'>
       Author: Henrik Moe
       </div>
@@ -14,7 +14,7 @@ const About = () => {
       </div>
 
       <div className='tradeSecretTitle'>
-      Protected under the U.S. Trade Secrets Act:
+      U.S. Trade Secrets Act:
       </div>
 
       <div className='aboutJinsei'>
@@ -34,10 +34,12 @@ const About = () => {
       <a href='#Groups'>Groups</a>
       </div>
 
-
+      <div className='tocItem'>
+      <a href='#Transactions'>Terms/Transasction</a>
+      </div>
 
       <div className='tocItem'>
-      <a href='#RiskManagement'>Risk Management</a>
+      <a href='#Exits'>Exits</a>
       </div>
 
       <div className='tocItem'>
@@ -45,12 +47,13 @@ const About = () => {
       </div>
 
       <div className='tocItem'>
-      <a href='#Transactions'>Transactions</a>
+      <a href='#Liabilities'>Liabilities</a>
+      </div>
+      <div className='tocItem'>
+      <a href='#RiskManagement'>Risk Management</a>
       </div>
 
-      <div className='tocItem'>
-      <a href='#Exits'>Exits</a>
-      </div>
+
 
       <div className='tocItem'>
       <a href='#Products'>Products</a>
@@ -60,7 +63,16 @@ const About = () => {
 Intro
       </h2>
       <div className='aboutJinsei2'>
-      Vertical talk, ai specialization products, rolling implementations and products
+      Vertical talk,
+      </div>
+      <div className='aboutJinsei2'>
+ai specialization products,
+</div>
+<div className='aboutJinsei2'>
+rolling implementations and
+</div>
+<div className='aboutJinsei2'>
+products
       </div>
 
       <h2 className='topicTitle' id='Groups'>
@@ -72,69 +84,189 @@ Intro
 
       </div>
 
-      <h2 className='topicTitle' id='RiskManagement'>
-      Risk Management
-      </h2>
-      <div className='aboutJinsei2'>
-      Distribution for them, roll over shares
-    Rollover equity refers to a scenario where the existing shareholders of a target company (the company being acquired) are offered the opportunity to exchange their shares for equity ownership in the acquiring company or the newly combined entity that results from the transaction
-    jinsei-quantexa corp shares to quantexa - for some shares in quantexa
-    to auditchain labs then contginetn that on a roll over with quantexa?
-    strucutre so multiple companies can enter for different product niches like iris and oxford and
-    Entity solution introduced here
-
-    ok so what does all these groups together look like? rolling partnership sandbox? auditchain with quantexa, oxford/dfrnt for maybe mapx
-i think focusing on auditchain quantexa and making it so i can leave and take it naic      rolling equity with a transaction fee towards research and implementation budget
-
-      </div>
-
-      <h2 className='topicTitle' id='Novelty'>
-Novelty
-      </h2>
-      <div className='aboutJinsei2'>
-      quantexa gets a 1 year exclusivity on jinsei Ip when the product lands in the vertical
-    whats auditchain’s vertical like? any? - for jason
-
-    making sure i can take this shit over to the naic
-
-
-      </div>
-
       <h2 className='topicTitle' id='Transactions'>
-Transaction
+    Terms/Transaction
       </h2>
       <div className='aboutJinsei2'>
       Two part (Triple part?) transaction where the first transactin is the acquistion of rollover equity and the second is distribution licensing upon vertical deployment per the novelty constraint
+      </div>
+
+      <div className='aboutJinsei2'>
       Transaction scenarios for each product vertical
+      </div>
+
+      <div className='aboutJinsei2'>
+
     Jinsei-Quantexa-Auditchain
-transaction for each entrerprise client for quantexa? no but this is lisecnse transaction?
-     rolling equity
+    </div>
+
+    <div className='aboutJinsei2'>
+
+    transaction for each entrerprise client for quantexa? no but this is lisecnse transaction?
+    </div>
+
+    <div className='aboutJinsei2'>
+rolling equity
+</div>
+
+<div className='aboutJinsei2'>
 
      Incentive from the next jinsei corp transaction with an enterprise distributor takes %25 licensing fee cut
+     </div>
 
+     <div className='aboutJinsei2'>
 
      rolling equity with a transaction fee towards research and implementation budget
-      </div>
+     </div>
+
+     <div className='aboutJinsei2'>
+
+     jv termsheet
+     </div>
+
+     <div className='aboutJinsei2'>
+
+     one or two classes of intetrest llc
+     </div>
+
+     <div className='aboutJinsei2'>
+
+     secutiries issued desrciption
+     </div>
+
+     <div className='aboutJinsei2'>
+
+     strucuture of entity
+     </div>
+
 
       <h2 className='topicTitle' id='Exits'>
       Exits
       </h2>
       <div className='aboutJinsei2'>
       exit plan for them
+</div>
+      <div className='aboutJinsei2'>
+
 Exit by the next enterprise vertical integration after 1 year from a vertical completion. Exit by distributor license via Quantexa, NAIC for next scale.
+</div>
+  <div className='aboutJinsei2'>
 novelty and protection
+</div>
+  <div className='aboutJinsei2'>
 client numbers and revenue data will help articulate the levearge of jinsei.ai’s ai products and report leveraging
-risk for risk roll over shares of their company for my company
+</div>
+  <div className='aboutJinsei2'>risk for risk roll over shares of their company for my company
 must reduce risk after first cash flow
+</div>
+  <div className='aboutJinsei2'>
 exit for early investors
+</div>
+  <div className='aboutJinsei2'>
 select product
+</div>
+  <div className='aboutJinsei2'>
 select one bunny
+</div>
+  <div className='aboutJinsei2'>
 the information i need is marketing data
+</div>
+  <div className='aboutJinsei2'>
 disappating risk in the corporation
+</div>
+  <div className='aboutJinsei2'>
 theyve got the distrubution i have the product
+</div>
+  <div className='aboutJinsei2'>
 theres no risk for them
+</div>
+  <div className='aboutJinsei2'>
 exclsuistivty after product landings
+</div>
+<div className='aboutJinsei2'>
+
+consideratin in exachange for each memeber contribution
+
       </div>
+
+
+      <h2 className='topicTitle' id='Novelty'>
+Novelty
+      </h2>
+      <div className='aboutJinsei2'>
+      quantexa gets a 1 year exclusivity on jinsei Ip when the product lands in the vertical
+</div>
+<div className='aboutJinsei2'>
+    whats auditchain’s vertical like? any? - for jason
+    </div>
+<div className='aboutJinsei2'>
+    making sure i can take this shit over to the naic
+      </div>
+
+
+      <h2 className='topicTitle' id='Liabilities'>
+      Liabilities
+      </h2>
+      <div className='aboutJinsei2'>
+      who would be responsible for what\
+      </div>
+      <div className='aboutJinsei2'>
+      projected costs of jv
+      </div>
+      <div className='aboutJinsei2'>
+      who would cover those costs
+      </div>
+      <div className='aboutJinsei2'>
+      quantexa’s resoruces ???
+      </div>
+      <div className='aboutJinsei2'>
+      whos contributing what
+      </div>
+
+
+
+      <h2 className='topicTitle' id='RiskManagement'>
+      Risk Management
+      </h2>
+      <div className='aboutJinsei2'>
+      Risk alignment
+      </div>
+      <div className='aboutJinsei2'>
+      Distribution for them, roll over shares
+      </div>
+      <div className='aboutJinsei2'>
+    Rollover equity refers to a scenario where the existing shareholders of a target company (the company being acquired) are offered the opportunity to exchange their shares for equity ownership in the acquiring company or the newly combined entity that results from the transaction
+</div>
+<div className='aboutJinsei2'>
+    jinsei-quantexa corp shares to quantexa - for some shares in quantexa
+</div>
+<div className='aboutJinsei2'>
+    to auditchain labs then contginetn that on a roll over with quantexa?
+</div>
+<div className='aboutJinsei2'>
+    strucutre so multiple companies can enter for different product niches like iris and oxford and
+</div>
+<div className='aboutJinsei2'>
+    Entity solution introduced here
+</div>
+<div className='aboutJinsei2'>
+    ok so what does all these groups together look like? rolling partnership sandbox? auditchain with quantexa, oxford/dfrnt for maybe mapx
+</div>
+<div className='aboutJinsei2'>
+i think focusing on auditchain quantexa and making it so i can leave and take it naic      rolling equity with a transaction fee towards research and implementation budget
+</div>
+<div className='aboutJinsei2'>
+
+Liability clause liability in the event of cash running
+</div>
+<div className='aboutJinsei2'>
+panel of represtnatives for the board
+</div>
+<div className='aboutJinsei2'>
+who would cover those costs
+
+      </div>
+
 
       <h2 className='topicTitle' id='Products'>
       Products
@@ -165,7 +297,7 @@ exclsuistivty after product landings
 
     <h3 className='productTitle'>
 
-    5 Entity and Infliction Graph Canvas
+    5 Triple Graph Canvas
     </h3>
 
     <h3 className='productTitle'>
@@ -189,6 +321,13 @@ exclsuistivty after product landings
     </h2>
 
     <div >
+    <div className='productSubTitle'>
+
+    operating budget
+    operating capital contribution
+
+
+  </div>
     <table className='xlsx-tablez'>
     <thead>
       <tr>
