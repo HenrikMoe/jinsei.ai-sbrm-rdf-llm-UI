3fa466afc1bc042f55e76225fe34ed31e96845b9
HenrikMoe <henrikmoe29@gmail.com>
2024-07-25 17:06:07 -0700

    endpoint and mayb service slection prop engineering needed

--- frontend/my-react-app/src/APIUIComponents/ServiceList.js
+++ frontend/my-react-app/src/APIUIComponents/ServiceList.js
@@ -1,28 +1,54 @@
 import React from 'react';
 
 const ServiceList = ({ SyntaxServices, AuthorityServices, onServiceClick }) => {
+  const commonItemStyle = {
+    border: '1px solid #ccc',
+    padding: '20px',
+    borderRadius: '8px',
+    marginTop: '10px',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer'
+  };
+
   return (
     <div>
-      <div style={{ marginBottom: '20px' }}>
-        <h3 style={{ color: 'tan' }}>Jinsei Syntax Transformers</h3>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-          {SyntaxServices.map((service, index) => (
-            <li key={index} style={{ marginBottom: '10px', cursor: 'pointer', color: 'white' }} onClick={() => onServiceClick(service)}>
-              {service.title}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }}>
+        <div>
+          <h3 style={{ color: 'tan' }}>ERP Transformers</h3>
+          <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '8px', color: 'tan' }}>
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+              {SyntaxServices.map((service, index) => (
+                <h3
+                  key={index}
+                  style={commonItemStyle}
+                  onClick={() => onServiceClick(service)}
+                >
+                  {service.title}
+                </h3>
+              ))}
+            </ul>
+          </div>
+        </div>
 
-      <div>
-        <h3 style={{ color: 'tan' }}>Jinsei Authority Transformers</h3>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-          {AuthorityServices.map((service, index) => (
-            <li key={index} style={{ marginBottom: '10px', cursor: 'pointer', color: 'white' }} onClick={() => onServiceClick(service)}>
-              {service.title}
-            </li>
-          ))}
-        </ul>
+        <div>
+          <h3 style={{ color: 'tan' }}>Authority Transformers</h3>
+          <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '8px', color: 'tan' }}>
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+              {AuthorityServices.map((service, index) => (
+                <h3
+                  key={index}
+                  style={commonItemStyle}
+                  onClick={() => onServiceClick(service)}
+                >
+                  {service.title}
+                </h3>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
