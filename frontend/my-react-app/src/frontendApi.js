1125a55aa23f3d4e7175afe10e249c44288d1419
HenrikMoe <henrikmoe29@gmail.com>
2024-09-04 17:22:08 -0700

    service infra????

--- frontend/my-react-app/src/frontendApi.js
+++ frontend/my-react-app/src/frontendApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
 
 const AI_API_BASE_URL = process.env.REACT_APP_AI_API_BASE_URL || 'http://localhost:8000';
-
+//oof
 
 console.log(API_BASE_URL)
 export const sendChatRequest = async (input) => {
